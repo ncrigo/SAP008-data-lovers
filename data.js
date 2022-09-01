8c462053b501ff3e57542ff751790a3cc6bf6743
ncrigo <ncrigobr@gmail.com>
2022-09-01 13:00:12 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,70 @@
+/* filtro por nome dos Pokémons */
+const sortByName = (data, valor) => {
+  const newData = [].concat(data);
+  if (valor === "A-Z") {
+    newData.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    ); /* localeCompare compara sequências em uma localidade específica */
+  } else {
+    newData.sort((a, b) => b.name.localeCompare(a.name));
+  }
+  return newData;
+};
+
+/* filtro pelo número do Pokémon */
+const sortByNum = (data, valor) => {
+  const newData = [].concat(data);
+  if (valor === "0-9") {
+    newData.sort((a, b) => {
+      /* sort para ordenar os items da array */
+      if (a.num > b.num) {
+        /* Se compare(a, b) for maior que zero, o método sort() classificará b com um índice menor que a, ou seja, b virá primeiro.*/
+        return 1;
+      } else {
+        /* e compare(a, b) for menor que zero, o método sort() classifica a para um índice menor que b. Ou seja, o a virá antes de b.*/
+        return -1;
+      }
+    });
+  } else {
+    newData.sort((a, b) => {
+      if (a.num < b.num) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+  return newData;
+};
+
+//* filtro por tipo *//
+const filterByType = (data, valor) => {
+  /* definindo os parâmetros */
+  const filterPkmnByType = data.filter(
+    /* abrindo uma const para receber o filtro */
+    (pokemon) =>
+      pokemon.type.indexOf(valor.toLowerCase()) >
+      -1 /* buscando valor que queremos pelo indexOf */
+  );
+  return filterPkmnByType;
+};
+
+const searchByName = (pokemon, name) => {
+  return pokemon.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(name.toLowerCase());
+  });
+};
+
+function percentageCalculation(filtered, all) {
+  const percentageType = (filtered * 100) / all;
+  const formattedPercentage = percentageType.toFixed(0);
+  return formattedPercentage;
+}
+
+export {
+  sortByName,
+  sortByNum,
+  filterByType,
+  searchByName,
+  percentageCalculation,
+};
---
+++ main.js
@@ -0,0 +1,183 @@
+import {
+  filterByType,
+  sortByName,
+  sortByNum,
+  searchByName,
+  percentageCalculation,
+} from "./data.js";
+
+import data from "./data/pokemon/pokemon.js";
+//* let para termos o datalist dos Pokémons *//
+let pkmnDataList = data.pokemon;
+
+/* Aparecer os pokemons no card */
+const scrollCard = document.getElementById("cardScroll");
+
+function pokemonList(pokemons) {
+  scrollCard.innerHTML = "";
+
+  pokemons.forEach((pokemon) => {
+    const card = document.createElement("div");
+
+    let typesPkmn = "";
+
+    pokemon.type.forEach((type) => {
+      typesPkmn += `<p class="type-style ${type}">${type}</p>`;
+    });
+
+    card.innerHTML = `
+    <div class="card">
+    <img src=${pokemon.img} alt="Foto pokémon">
+    <p class="number-style">${pokemon.num}</p>
+    <p class="name-style">${pokemon.name}</p>    
+    <div class="type-teste">${typesPkmn}</div>
+    </div>
+    `;
+
+    card.addEventListener("click", function () {
+      openModal(pokemon);
+    });
+
+    scrollCard.appendChild(card);
+  });
+}
+pokemonList(pkmnDataList);
+
+/* Acionando os filtros */
+const alphabeticOrdenation = document.getElementById("sortName");
+const numberAscDescOrdenation = document.getElementById("sortNum");
+const typeFilter = document.getElementById("typeFilter");
+
+alphabeticOrdenation.addEventListener("change", function () {
+  let sortedPokemons = [];
+  if (alphabeticOrdenation.value == "A-Z") {
+    sortedPokemons = sortByName(pkmnDataList, "A-Z");
+  } else {
+    sortedPokemons = sortByName(pkmnDataList, "Z-A");
+  }
+  pokemonList(sortedPokemons);
+});
+
+numberAscDescOrdenation.addEventListener("change", function () {
+  let sortedPokemons = [];
+  if (numberAscDescOrdenation.value == "0-9") {
+    sortedPokemons = sortByNum(pkmnDataList, "0-9");
+  } else {
+    sortedPokemons = sortByNum(pkmnDataList, "9-0");
+  }
+  pokemonList(sortedPokemons);
+});
+
+typeFilter.addEventListener("change", function () {
+  pkmnDataList = data.pokemon;
+
+  const typePercentage = document.getElementById("typePercentageCard");
+
+  let filteredPokemons = [];
+  if (typeFilter.value != "all") {
+    filteredPokemons = filterByType(pkmnDataList, typeFilter.value);
+
+    const percentageValue = percentageCalculation(
+      filteredPokemons.length,
+      pkmnDataList.length
+    );
+
+    typePercentage.style.display = "block";
+    typePercentage.innerHTML = `O tipo ${typeFilter.value} representa ${percentageValue}% dos pokémons da primeira e segunda geração!`;
+  } else {
+    typePercentage.style.display = "none";
+    filteredPokemons = pkmnDataList;
+  }
+  pokemonList(filteredPokemons);
+});
+
+document
+  .getElementById("searchByNameInput")
+  .addEventListener("keyup", searchName);
+
+function searchName(evento) {
+  const name = evento.target.value;
+  const result = searchByName(data.pokemon, name);
+  pokemonList(result);
+}
+
+const clearButton = document.getElementById("cleanButton");
+
+function cleanInput() {
+  FormData.reset();
+}
+clearButton.addEventListener("click", cleanInput);
+
+/*Modal*/
+function openModal(pokemon) {
+  const modal = document.getElementById("modalKnowMore");
+
+  if (typeof modal == "undefined" || modal === null) {
+    return;
+  }
+
+  let typesPkmn = "";
+  let resistantPkmn = "";
+  let weaknessesPkmn = "";
+
+  pokemon.type.forEach((type) => {
+    typesPkmn += `<p class="type-style ${type}">${type}</p>`;
+  });
+
+  pokemon.resistant.forEach((resistant) => {
+    resistantPkmn += `<p class="type-style ${resistant}">${resistant}</p>`;
+  });
+
+  pokemon.weaknesses.forEach((weaknesses) => {
+    weaknessesPkmn += `<p class="type-style ${weaknesses}">${weaknesses}</p>`;
+  });
+
+  const modalContent = document.getElementById("modalContent");
+
+  modalContent.innerHTML = `
+    <div class="modal-body">
+      <div class="card-modal">
+        <img src=${pokemon.img} alt="Foto pokémon">
+        <p class="number-style">${pokemon.num}</p>
+        <p class="name-style">${pokemon.name}</p>    
+        <div class="type-teste">${typesPkmn}</div>
+      </div>
+
+      <div class="modal-stats">
+        <p class="modal-height">Altura: ${pokemon.size.height}</p>
+        <p class="modal-weight">Peso: ${pokemon.size.weight}</p> <br>
+        <p>Ataque: ${pokemon.stats["base-attack"]}</p>
+        <p>Defesa: ${pokemon.stats["base-defense"]}</p>
+        <p>Energia: ${pokemon.stats["base-stamina"]}</p>
+        <p>HP: ${pokemon.stats["max-hp"]}</p> <br>
+      </div>
+    </div>
+     
+    <div class="modal-types"> 
+      <p class="modal-types-title">Resistências:</p>
+      <div class="modal-types-display">${resistantPkmn}</div>
+    </div>
+    
+    <div class="modal-types"> 
+      <p class="modal-types-title">Fraquezas:</p>
+      <div class="modal-types-display">${weaknessesPkmn}</div>
+    </div>
+  `;
+
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+
+const button = document.getElementById("closeModalKnowMore");
+button.addEventListener("click", closeModal);
+
+function closeModal() {
+  const modal = document.getElementById("modalKnowMore");
+
+  if (typeof modal == "undefined" || modal === null) {
+    return;
+  }
+
+  modal.style.display = "none";
+  document.body.style.overflow = "auto";
+}
